a5d7a84a68a67dcdd36ade72d634bf114ada3d25
SriPauldaniel <91692450+SriPauldaniel@users.noreply.github.com>
2023-07-14 12:56:29 +0530

    Update EquilibriumIndex.java

--- EquilibriumIndex.java
+++ EquilibriumIndex.java
@@ -14,15 +14,21 @@ public class EquilibriumIndex {
         int[] arr1 = new int[n];
         int sum = arr[0];
         arr1[0] = sum;
-        for(int i=1;i<n;i++) {
+        for(int i=1;i<n-1;i++) {
             sum += arr[i];
             arr1[i] = sum;
         }
-
-        for(int i=1;i<n-1;i++) {
+        int i=0;
+        if(arr1[n-1]-arr1[i] == 0){
+            return 0;
+        }
+        for(i=1;i<n;i++) {
             if(arr1[i-1] == arr1[n-1]-arr1[i]) {
                 return i;
             }
+        }
+        if(arr1[n-2] == 0) {
+            return n-1;
         }
         return -1;
     }
